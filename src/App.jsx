6efb06ad40a32bcd1688016dcb54d63babae16b2
Samiul Hoque Limo <samiul.limo@gmail.com>
2023-07-18 16:17:41 +0600

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import {IoMdAddCircleOutline} from "react-icons/io";
+import {FiDelete} from "react-icons/fi";
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [todos, setTodos] = useState(() => []);
 
-    return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleAddTodo = () => {
+        if (inputValue.trim() === "") {
+            return;
+        }
+        setTodos((currentTodos) => {
+            return [...currentTodos, {id: Math.random() * 1234, text: inputValue, completed: false},];
+        });
+        setInputValue("");
+    };
+
+    const handleDeleteTodo = (id) => {
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+    };
+
+    const toggleTodo = (id, completed) => {
+        setTodos((currentTodos) => {
+            return currentTodos.map((todo) => {
+                if (todo.id === id) {
+                    return {...todo, completed};
+                }
+                return todo;
+            });
+        });
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            handleAddTodo();
+        }
+    };
+
+    const filteredTodos = todos ? todos.filter((todo) => todo.completed !== false) : [];
+
+    return (<section id="todoList">
+        <header>
+            <h1>Todo App</h1>
+            <div className="todo-content">
+                <p>Completed: {filteredTodos.length}/{todos.length}</p>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    )
+        </header>
+
+        <div className="input">
+            <button onClick={handleAddTodo}><IoMdAddCircleOutline/></button>
+            <input
+                placeholder="Enter your todo here..."
+                type="text"
+                value={inputValue}
+                onKeyDown={handleKeyDown}
+                onChange={handleInputChange}
+            />
+        </div>
+
+        <ul>
+            {0 === todos.length &&
+                <p style={{textAlign: "center", padding: "10px", borderBottom: "1px solid gainsboro"}}>
+                    Your don't have any to do items
+                </p>}
+
+            {todos.map((todo, index) => (<li key={index}>
+            <span className={todo.completed ? "completed" : ""}>
+
+              <input id="checkbox" type="checkbox" checked={todo.completed}
+                     onChange={(e) => toggleTodo(todo.id, e.target.checked)}/>
+                {todo.text}
+
+            </span>
+                <button className="danger" onClick={() => handleDeleteTodo(todo.id)}><FiDelete/></button>
+            </li>))}
+        </ul>
+    </section>);
 }
 
-export default App
+export default App;
